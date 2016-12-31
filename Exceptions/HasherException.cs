7c46a15f79726ca6b05b4a5511ffcb13f200a97e
Truong Nguyen <samuraitruong@hotmail.com>
2016-12-31 18:18:08 +1100

    Add exception handle throw to bot layer for friendly message

---
+++ Exceptions/HasherException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonGo.RocketAPI.Exceptions
+{
+    public class HasherException :Exception
+    {
+        public HasherException(string message): base(message)
+        {
+
+        }
+    }
+}
