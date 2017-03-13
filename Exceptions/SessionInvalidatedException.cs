ad24360057716b0b96911cbffa2dbb4450d0da9d
Skuld <jjskuld@gmail.com>
2017-03-13 03:41:20 -0700

    Add new SessionInvalidatedException

---
+++ Exceptions/SessionInvalidatedException.cs
@@ -0,0 +1,20 @@
+﻿#region using directives
+
+using System;
+
+#endregion
+
+namespace PokemonGo.RocketAPI.Exceptions
+{
+    
+    public class SessionInvalidatedException : Exception
+    {
+        public SessionInvalidatedException()
+        {
+        }
+
+        public SessionInvalidatedException(string message) : base(message)
+        {
+        }
+    }
+}
