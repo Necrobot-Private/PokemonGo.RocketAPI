833c1c4eb9b2e832461b5606870b0f9c37f9dac1
--=FurtiF™=-- <Furtif@users.noreply.github.com>
2017-04-08 22:29:32 +0200

    Update Client.cs

--- Client.cs
+++ Client.cs
@@ -98,7 +98,8 @@ namespace PokemonGo.RocketAPI
                 Hasher = new PokefamerHasher(settings.AuthAPIKey, settings.DisplayVerboseLog, ApiEndPoint);
 
                 // These 4 constants below need to change if we update the hashing server API version that is used.
-                //Unknown25 = -816976800928766045; // 0.59.1
+                //Unknown25 = -816976800928766045; // 0.45.0
+		//Unknown25 = -3226782243204485589; // 0.59.1
                 Unknown25 = 1296456256998993698; // 0.61.0
 				
                 // WARNING! IF YOU CHANGE THE APPVERSION BELOW ALSO UPDATE THE API_VERSION AT THE TOP OF THE FILE!
