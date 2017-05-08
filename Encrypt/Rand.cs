8825cd1b6d2281b0cfd9cce850ea9f17f75f4de7
Skuld <jjskuld@gmail.com>
2017-05-07 18:03:24 -0700

    Add new cipher encryption.  Credits to Xelwon of Pokemaster Bot (https://github.com/Logxn/PokemonGo-Bot/commit/856dd12255dd5a940b03ee802bcb024d48e9820f)

---
+++ Encrypt/Rand.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Xelwon
+ * Date: 08/05/2017
+ * Time: 1:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace PokemonGo.RocketAPI.Encrypt
+{
+    /// <summary>
+    /// Description of Rand.
+    /// </summary>
+    public class Rand
+    {
+        private long state;
+
+        public Rand(long state)
+        {
+            this.state = state;
+        }
+
+        public byte Next()
+        {
+            state = (state * 0x41C64E6D) + 0x3039;
+            return (byte)((state >> 16) & 0xFF);
+        }
+    }
+
+}
